bc084a84c7487a6a4bb8f8f5a1261ad67d95e9cb
albafica <albaficaw@gmail.com>
2015-01-27 22:23:31 +0800

    新需求流程

---
+++ Document/需求1.1/data/sitemap.js
@@ -0,0 +1,8 @@
+﻿var sitemap = 
+
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j),_(c,k,e,f,g,l,m,[_(c,n,e,f,g,o),_(c,p,e,f,g,q)]),_(c,r,e,f,g,s,m,[_(c,t,e,f,g,u)])]);}; 
+var b="rootNodes",c="pageName",d="简历流程",e="type",f="Wireframe",g="url",h="简历流程.html",i="企业流程",j="企业流程.html",k="后台管理",l="后台管理.html",m="children",n="企业管理",o="企业管理.html",p="简历管理",q="简历管理.html",r="首页",s="首页.html",t="简历页",u="简历页.html";
+return _creator();
+})();
---
+++ Document/需求1.1/简历页_files/axurerp_pagespecificscript.js
@@ -0,0 +1,7 @@
+﻿for(var i = 0; i < 19; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u16'] = 'top';gv_vAlignTable['u12'] = 'center';gv_vAlignTable['u15'] = 'top';gv_vAlignTable['u4'] = 'top';gv_vAlignTable['u8'] = 'top';gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u14'] = 'top';gv_vAlignTable['u6'] = 'center';gv_vAlignTable['u13'] = 'top';gv_vAlignTable['u18'] = 'top';gv_vAlignTable['u7'] = 'top';
