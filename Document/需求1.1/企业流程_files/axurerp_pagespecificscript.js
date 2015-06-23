9cc5e318949dab6fe1086735aa07c91df55ae301
albafica <albaficaw@gmail.com>
2015-06-23 13:37:51 +0800

    1、排除文档内容，避免泄露

--- Document/需求1.1/企业流程_files/axurerp_pagespecificscript.js
+++
@@ -1,7 +0,0 @@
-﻿for(var i = 0; i < 50; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
-
-$axure.eventManager.pageLoad(
-function (e) {
-
-});
-gv_vAlignTable['u31'] = 'center';gv_vAlignTable['u17'] = 'center';gv_vAlignTable['u29'] = 'center';gv_vAlignTable['u21'] = 'center';gv_vAlignTable['u15'] = 'top';gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u14'] = 'top';gv_vAlignTable['u43'] = 'center';gv_vAlignTable['u37'] = 'center';gv_vAlignTable['u41'] = 'center';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u3'] = 'top';gv_vAlignTable['u39'] = 'center';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u35'] = 'center';gv_vAlignTable['u27'] = 'center';gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u23'] = 'center';gv_vAlignTable['u25'] = 'center';gv_vAlignTable['u2'] = 'center';gv_vAlignTable['u19'] = 'center';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u49'] = 'center';gv_vAlignTable['u47'] = 'center';gv_vAlignTable['u45'] = 'center';gv_vAlignTable['u33'] = 'center';gv_vAlignTable['u0'] = 'top';
--- Document/需求1.1/首页_files/axurerp_pagespecificscript.js
+++
@@ -1,7 +0,0 @@
-﻿for(var i = 0; i < 61; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
-
-$axure.eventManager.pageLoad(
-function (e) {
-
-});
-gv_vAlignTable['u31'] = 'top';gv_vAlignTable['u36'] = 'center';gv_vAlignTable['u16'] = 'top';gv_vAlignTable['u29'] = 'center';gv_vAlignTable['u8'] = 'top';gv_vAlignTable['u30'] = 'top';gv_vAlignTable['u57'] = 'top';gv_vAlignTable['u32'] = 'top';gv_vAlignTable['u59'] = 'top';gv_vAlignTable['u60'] = 'top';gv_vAlignTable['u15'] = 'center';gv_vAlignTable['u13'] = 'top';gv_vAlignTable['u38'] = 'center';gv_vAlignTable['u43'] = 'top';gv_vAlignTable['u40'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u12'] = 'top';gv_vAlignTable['u27'] = 'center';gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u42'] = 'center';gv_vAlignTable['u58'] = 'top';gv_vAlignTable['u24'] = 'top';gv_vAlignTable['u46'] = 'top';gv_vAlignTable['u53'] = 'top';gv_vAlignTable['u56'] = 'top';gv_vAlignTable['u54'] = 'top';gv_vAlignTable['u2'] = 'top';gv_vAlignTable['u18'] = 'center';gv_vAlignTable['u19'] = 'top';gv_vAlignTable['u55'] = 'top';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u48'] = 'top';gv_vAlignTable['u47'] = 'top';gv_vAlignTable['u50'] = 'center';gv_vAlignTable['u45'] = 'center';gv_vAlignTable['u52'] = 'center';gv_vAlignTable['u34'] = 'center';
